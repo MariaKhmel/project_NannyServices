1a3365a9eaba9e74793f942256d3766f8d30286e
Maria Khmel <mariakhmel8@gmail.com>
2024-10-27 03:07:47 +0200

    app

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,5 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import AppBar from "../components/AppBar/AppBar";
+import HomePage from "../pages/HomePage/HomePage";
+
 function App() {
-  return <>app</>;
+  return (
+    <>
+      <AppBar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
