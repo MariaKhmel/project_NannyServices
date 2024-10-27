92feb726f04f7277ffd1e2f7da9dc0c519b75785
Maria Khmel <mariakhmel8@gmail.com>
2024-10-27 03:08:17 +0200

    home

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,15 @@
+import { IoMdArrowRoundForward } from "react-icons/io";
+
+const HomePage = () => {
+  return (
+    <>
+      <h1>Make Life Easier for the Family:</h1>
+      <p>Find Babysitters Online for All Occasions</p>
+      <button type="button">
+        Get started <IoMdArrowRoundForward />
+      </button>
+    </>
+  );
+};
+
+export default HomePage;
