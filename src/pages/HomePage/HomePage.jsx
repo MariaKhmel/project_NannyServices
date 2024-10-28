cf802082cd6675c98a4272d5ab9eddb226fe4239
Maria Khmel <mariakhmel8@gmail.com>
2024-10-28 02:39:21 +0200

    form

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,13 @@
 import { IoMdArrowRoundForward } from "react-icons/io";
+import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
+  const navigate = useNavigate();
   return (
     <>
       <h1>Make Life Easier for the Family:</h1>
       <p>Find Babysitters Online for All Occasions</p>
-      <button type="button">
+      <button type="button" onClick={navigate()}>
         Get started <IoMdArrowRoundForward />
       </button>
     </>
