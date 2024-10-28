89fba460c908d790f6ca7b4e17cb9b6609d91b79
Maria Khmel <mariakhmel8@gmail.com>
2024-10-28 21:03:43 +0200

    registration

--- src/components/Registration/Registration.jsx
+++ src/components/Registration/Registration.jsx
@@ -1,6 +1,13 @@
+import { useState } from "react";
 import css from "./Registration.module.css";
 
 const SignInForm = () => {
+  const [formValues, setFormValues] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
   const handleFormSubmit = (e) => {
     e.preventDefault();
     const form = e.target;
@@ -9,6 +16,13 @@ const SignInForm = () => {
     const password = form.elements.password.value;
     console.log({ name, email, password });
     form.reset();
+  };
+
+  const handleFormValuesChange = (e) => {
+    setFormValues({
+      ...formValues,
+      [e.target.name]: e.target.value,
+    });
   };
   return (
     <div className={css.modalOverlay}>
@@ -20,9 +34,27 @@ const SignInForm = () => {
           information.
         </p>
         <form onSubmit={handleFormSubmit}>
-          <input type="text" name="name" placeholder="Name" />
-          <input type="email" name="email" placeholder="Email" />
-          <input type="password" name="password" placeholder="Password" />
+          <input
+            type="text"
+            name="name"
+            placeholder="Name"
+            value={formValues.name}
+            onChange={handleFormValuesChange}
+          />
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            value={formValues.email}
+            onChange={handleFormValuesChange}
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={formValues.password}
+            onChange={handleFormValuesChange}
+          />
           <button type="sumbit">Sign Up</button>
         </form>
       </div>
