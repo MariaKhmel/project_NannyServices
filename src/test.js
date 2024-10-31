cb4f97c2c4c522f8e3f148675ce0d9f068160b6d
Maria Khmel <mariakhmel8@gmail.com>
2024-10-31 03:11:43 +0200

    append

--- src/test.js
+++ src/test.js
@@ -1,9 +1,55 @@
-const liArr = document.querySelectorAll('.item');
-console.log(`Number of categories: ${liArr.length}`)
+// const liArr = document.querySelectorAll('.item');
+// console.log(`Number of categories: ${liArr.length}`)
 
-liArr.forEach(el => {
-  const h2 = el.querySelector('h2').textContent;
-  const liQuantity = el.querySelectorAll('li').length;
-  console.log(`Category: ${h2}`);
-  console.log(`Elements : ${liQuantity}`)
+// liArr.forEach(el => {
+//   const h2 = el.querySelector('h2').textContent;
+//   const liQuantity = el.querySelectorAll('li').length;
+//   console.log(`Category: ${h2}`);
+//   console.log(`Elements : ${liQuantity}`)
+// });
+
+const images = [
+  {
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
+  },
+  {
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+  },
+  {
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
+  },
+  {
+    url: "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg",
+    alt: "Alpine Spring Meadows",
+  },
+  {
+    url: "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg",
+    alt: "Nature Landscape",
+  },
+  {
+    url: "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
+    alt: "Lighthouse Coast Sea",
+  },
+];
+
+const galleryRef = document.querySelector('.gallery');
+
+
+const markUp = images.map(({ url, alt }) => {
+  const li = document.createElement("li");
+  const img = document.createElement("img");
+  img.alt = alt;
+  img.src = url;
+  img.width = 300;
+  img.height = 200;
+  li.append(img);
+  return li;
 });
+
+
+galleryRef.append(...markUp);
+
+
