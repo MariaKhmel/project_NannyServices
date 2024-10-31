2d9de7abb88815789a51eb4eb6f8d1d862aff720
Maria Khmel <mariakhmel8@gmail.com>
2024-10-31 03:35:21 +0200

    color

--- src/test.js
+++ src/test.js
@@ -82,3 +82,19 @@ const images = [
 // }
 
 // form.addEventListener('submit', onFormSubmitHandler)
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const changeColorBtn = document.querySelector('.change-color')
+const colorSpan = document.querySelector('.color');
+const body = document.querySelector('body');
+
+changeColorBtn.addEventListener('click', () => {
+  const color = getRandomHexColor();
+  colorSpan.textContent = color;
+  body.style.backgroundColor = color;
+})
