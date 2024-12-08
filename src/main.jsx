7d6eeb9086578a39af37e187f08c5979fc7bc957
Maria Khmel <mariakhmel8@gmail.com>
2024-12-09 01:10:00 +0200

    fx

--- src/main.jsx
+++ src/main.jsx
@@ -5,19 +5,6 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import App from "./components/App/App";
 
-function toCamelCase(str) {
-  const delimeters = ["-", "_"];
-  const delimeter = delimeters.find((d) => str.includes(d));
-  return str
-    .split(delimeter)
-    .map((word, index) =>
-      index === 0 ? word : word[0].toUpperCase() + word.slice(1)
-    )
-    .join("");
-}
-
-console.log(toCamelCase("the_stealth_warrior"));
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
   <BrowserRouter>
