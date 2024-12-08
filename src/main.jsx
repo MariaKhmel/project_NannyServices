18133a5e0c11891d1bb635a99f64cabdb991c5c0
Maria Khmel <mariakhmel8@gmail.com>
2024-12-08 03:22:24 +0200

    tower

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,29 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import App from "./components/App/App";
 
+function towerBuilder(nFloors) {
+const tower = [];
+if(nFloors<1){
+  return;
+}
+for(let i=1; i<=nFloors;i+=1){
+let stars = '';
+let spaces = '';
 
+for(let j = 0 ; j < i*2-1; j+=1){
+  stars+='*';
+}
+
+
+for(let k = 0; k<nFloors-1; k+=1){
+  spaces+='';
+}
+tower.push(spaces,stars,spaces)
+}
+return tower;
+  }
+
+  console.log(towerBuilder(2))
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
