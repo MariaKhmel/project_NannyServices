01e50b6722d11245a52291ea73e300d2210deb7e
Maria Khmel <mariakhmel8@gmail.com>
2024-12-09 00:43:01 +0200

    tocamelcase

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,20 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import App from "./components/App/App";
 
+function toCamelCase(str) {
+  if (str.includes("_") || str.includes("-")) {
+    const upperCaseArr = str.split("_").map((word, i, arr) => {
+      if (arr[0][0] === arr[0][0].toUpperCase()) {
+        const firstLetter = word[0].toUpperCase();
+        const restOfTheWord = arr[i].slice(1);
+        return [...firstLetter, ...restOfTheWord].join("");
+      }
+    });
+    return upperCaseArr.join("");
+  }
+}
 
-
+console.log(toCamelCase("the_stealth_warrior"));
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
