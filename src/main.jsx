88ea91bfab3137bd2aa5dd14ae99a8ecf4814b9b
Maria Khmel <mariakhmel8@gmail.com>
2024-12-08 02:53:41 +0200

    isTriangle

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,17 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import App from "./components/App/App";
 
+function isTriangle(a, b, c) {
+  // Check if all sides are positive
+  if (a <= 0 || b <= 0 || c <= 0) {
+    return false;
+  }
 
+  // Check the triangle inequality theorem
+  return a + b > c && b + c > a && a + c > b;
+}
+
+console.log(isTriangle(2,6,7))
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
