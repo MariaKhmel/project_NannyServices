7648e229a4e2f162ebf60a787fbb1d3fd42b3b49
Maria Khmel <mariakhmel8@gmail.com>
2024-12-09 02:46:46 +0200

    fx

--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,6 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import App from "./components/App/App";
 
-function alphabetPosition(text) {
-  return text
-    .toLowerCase()
-    .split("")
-    .filter((char) => /[a-z]/.test(char))
-    .map((char) => char.charCodeAt(0) - 96)
-    .join(" ");
-}
-console.log(alphabetPosition("The sunset sets at twelve o' clock."));
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
   <BrowserRouter>
