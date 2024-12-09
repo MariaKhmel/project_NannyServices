2513d05c3a5118703a3638bc61b33a4b862402b2
Maria Khmel <mariakhmel8@gmail.com>
2024-12-09 02:06:53 +0200

    twoSum

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,19 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import App from "./components/App/App";
 
+function twoSum(numbers, target) {
+  for (let i = 0; i < numbers.length; i += 1) {
+    for (let j = i + 1; j < numbers.length; j += 1) {
+      if (numbers[i] + numbers[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+}
+console.log(twoSum([1, 2, 3], 4));
+console.log(twoSum([1234, 5678, 9012], 14690));
+console.log(twoSum([2, 2, 3], 4));
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
   <BrowserRouter>
