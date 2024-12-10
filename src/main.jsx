159d0c6abd4e74ceea227e67c7acbb9d606440f2
Maria Khmel <mariakhmel8@gmail.com>
2024-12-10 23:46:23 +0200

    uniqueInOrder

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,10 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import App from "./components/App/App";
 
+var uniqueInOrder = function (iterable) {
+  //your code here - remember iterable can be a string or an array
+};
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
   <BrowserRouter>
