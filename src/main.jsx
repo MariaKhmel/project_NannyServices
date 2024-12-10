b5918a9102553c5082c6f02c4aeeafbdd05ff5fe
Maria Khmel <mariakhmel8@gmail.com>
2024-12-11 00:57:47 +0200

    fx

--- src/main.jsx
+++ src/main.jsx
@@ -5,19 +5,6 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import App from "./components/App/App";
 
-var uniqueInOrder = function (iterable) {
-  if (typeof iterable === "string") {
-    const arr = iterable.split("");
-    return arr.filter((char, i, arr) => char !== arr[i + 1]);
-  } else if (typeof iterable === "object") {
-    return iterable.filter((char, i, arr) => char !== arr[i + 1]);
-  }
-};
-
-console.log(uniqueInOrder("AAAABBBCCDAABBB"));
-console.log(uniqueInOrder("ABBCcAD"));
-console.log(uniqueInOrder([1, 2, 2, 3, 3]));
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
   <BrowserRouter>
