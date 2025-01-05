2e76031e43d895a05a026545c516ff92acbe112b
Maria Khmel <mariakhmel8@gmail.com>
2025-01-05 04:35:53 +0200

    app

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -18,6 +18,7 @@ function App() {
       setIsLogInModalShown(true);
     }
   }, [location.pathname]);
+
   return (
     <>
       <AppBar />
--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -2,8 +2,10 @@ import { NavLink } from "react-router-dom";
 import AuthNav from "../AuthNav/AuthNav";
 import Navigation from "../Navigation/Navigation";
 import css from "./AppBar.module.css";
+import { useState } from "react";
 
 const AppBar = () => {
+  const [selected, isSelected] = useState("");
   return (
     <header className={css.header}>
       <NavLink to="/" className={css.logo}>
