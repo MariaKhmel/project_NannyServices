d425b366622ca1c2a8de20f6f2d8504d46f54756
Maria Khmel <mariakhmel8@gmail.com>
2025-01-06 04:23:06 +0200

    fx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,14 @@ import "modern-normalize";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import App from "./components/App/App";
-import Bugsnag from "./bugsnag"; // Import your Bugsnag initialization
+import Bugsnag from "@bugsnag/js";
+import BugsnagPluginReact from "@bugsnag/plugin-react";
+
+Bugsnag.start({
+  apiKey: "06cbef00e810fbbce732932e3abfdb68", // Replace with your Bugsnag API key
+  plugins: [new BugsnagPluginReact()],
+});
+
 const ErrorBoundary = Bugsnag.getPlugin("react")?.createErrorBoundary(React);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
