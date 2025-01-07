6eb992056b2f4ae2a2dbe8138be9ec8cea957661
Maria Khmel <mariakhmel8@gmail.com>
2025-01-07 21:53:40 +0200

    fx

--- src/main.jsx
+++ src/main.jsx
@@ -4,22 +4,13 @@ import "modern-normalize";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import App from "./components/App/App";
-import Bugsnag from "@bugsnag/js";
-import BugsnagPluginReact from "@bugsnag/plugin-react";
-
-Bugsnag.start({
-  apiKey: "06cbef00e810fbbce732932e3abfdb68", // Replace with your Bugsnag API key
-  plugins: [new BugsnagPluginReact()],
-});
-
-const ErrorBoundary = Bugsnag.getPlugin("react")?.createErrorBoundary(React);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
-  <ErrorBoundary>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </ErrorBoundary>
+
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+
   // </React.StrictMode>
 );
