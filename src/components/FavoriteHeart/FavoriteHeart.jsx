8c101289ee7eeeb41c9b8c82174570ce8e0303b5
Maria Khmel <mariakhmel8@gmail.com>
2025-01-31 21:55:13 +0200

    favotites

--- src/components/FavoriteHeart/FavoriteHeart.jsx
+++ src/components/FavoriteHeart/FavoriteHeart.jsx
@@ -2,11 +2,16 @@ import { FaRegHeart, FaHeart } from "react-icons/fa";
 import css from "./FavoriteHeart.module.css";
 
 const FavoriteHeart = ({ isFavorite, setIsFavorite }) => {
-  const onSetFavoriteBtnClick = () => {
-    setIsFavorite((prevValue) => !prevValue);
-  };
+  // const onSetFavoriteBtnClick = () => {
+  //   setIsFavorite((prevValue) => !prevValue);
+  // };
   return (
-    <button onClick={onSetFavoriteBtnClick} className={css.favoriteHeartBtn}>
+    <button
+      onClick={() => {
+        setIsFavorite((prevValue) => !prevValue);
+      }}
+      className={css.favoriteHeartBtn}
+    >
       {isFavorite ? <FaHeart /> : <FaRegHeart />}
     </button>
   );
